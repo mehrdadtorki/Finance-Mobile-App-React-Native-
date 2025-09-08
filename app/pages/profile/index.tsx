b5a28e4c5559e4a3e3310334959c1fdb7a407b04
mehrdadtorki <mehrdadtorki1381@gmail.com>
2025-09-08 13:43:54 +0330

    implement basic UI for personal page

--- app/pages/profile/index.tsx
+++ app/pages/profile/index.tsx
@@ -1,15 +1,81 @@
+import Constants from "expo-constants";
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+
+const screen = Dimensions.get("screen");
 
 export default function Profile() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Profile Page</Text>
+      <View style={styles.header}>
+        <View style={styles.setting}>
+          {/* <TouchableOpacity onPress={() => {}}> */}
+          <FontAwesome name="bell-o" size={20} color="white" />
+          {/* </TouchableOpacity> */}
+        </View>
+        <View>
+          <Text style={styles.pageTitle}>Profile</Text>
+        </View>
+      </View>
+      {/* Content can go here */}
+      <View style={styles.content}>
+        <View style={styles.profileContainer}>
+          <Text style={styles.username}></Text>
+          <Text style={styles.email}></Text>
+        </View>
+        {/* <View style={styles.bannerContainer}></View>
+        <View style={styles.accountDetailsContainer}></View> */}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
-  text: { fontSize: 20, color: "#e91b1bff" },
+  container: {
+    flex: 1,
+    paddingTop: Constants.statusBarHeight,
+    alignItems: "center",
+    backgroundColor: "#6172F3",
+  },
+  header: {
+    width: "100%",
+    paddingVertical: 24,
+    // paddingHorizontal: 20,
+    // paddingBottom: 20,
+    display: "flex",
+    flexDirection: "row-reverse",
+    justifyContent: "space-between",
+  },
+  setting: {
+    padding: 16,
+  },
+  pageTitle: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: 600,
+  },
+  content: {
+    flex: 1,
+    backgroundColor: "#fff",
+    height: screen.height * 0.75,
+    width: screen.width,
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 24,
+  },
+  profileContainer: {
+    display: "flex",
+    flexDirection: "column",
+    rowGap: 12,
+  },
+  username: {
+    fontSize: 20,
+    fontWeight: "600",
+    color: "#101828",
+  },
+  email: {
+    fontSize: 14,
+    fontWeight: "400",
+    color: "#475467",
+  },
 });
